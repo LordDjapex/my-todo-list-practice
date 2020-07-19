151ea32ec1e3424415658269456484c95aa24286
Djapex <nikolapudja@gmail.com>
2020-07-19 16:47:35 +0200

    commit2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = {
+        todolist: [
+            {id: Math.random(), content:'Content'},
+            {id: Math.random(), content:'Nigga'},
+            {id: Math.random(), content:'Content'}
+        ]
+    }
+
+
+
+    addTask = (e) => {
+        e.preventDefault()
+        let input = document.querySelector('#inputer').value
+        const id = Math.random()
+        this.setState({
+            todolist: [...this.state.todolist, {id: id, content: input}]
+        })
+    }
+
+    deleteTask = (id) => {
+        this.setState({
+            todolist: this.state.todolist.filter((item) => {
+                return item.id !== id
+            })
+        })
+    }
+    
+
+    render() {
+        return (
+            <>
+            <ul id="list">
+                    {this.state.todolist.map((item) => (<li key={item.id}>{item.content} <button onClick={() => this.deleteTask(item.id)}>X</button></li> ))}
+            </ul>
+            <form onSubmit={(e) => this.addTask(e)}>
+                <label name="content-input">Input content:</label>
+                <input name="content-input" id="inputer" placeholder="input content" type="text" required></input>
+                <button type="submit">submit</button>
+            </form>
+            </>
+        )
+    }
 }
-
-export default App;
